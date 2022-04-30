5de5eb6c8a18a6ba14ff044a5913d717ae095ec4
Nicolas Robles <nicolasrobles@MacBook-Pro-de-Nicolas.local>
2022-04-30 02:41:14 -0300

    rediseño estético

--- src/js/generadorCards.js
+++ src/js/generadorCards.js
@@ -8,7 +8,7 @@ const generarCardsHTML = (arrayARecorrer) => {
         acumulador += `
         <div class="card h-100 mb-3">
         <!-- Etiqueta de producto en "Sale" -->
-        <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Black-Friday</div>
+        <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">20% OFF</div>
         <!-- Imagen del producto -->
         <img class="card-img-top mt-5" src="${producto.imagen}" alt="..." />
         <!-- Detalles del producto -->
@@ -25,12 +25,12 @@ const generarCardsHTML = (arrayARecorrer) => {
             <div class="text-center">
             <button 
             onclick="agregarAlCarrito(${producto.id})"
-            class="btn btn btn-primary mt-auto" href="#">
+            class="btn btn btn-warning mt-auto" href="#">
             Agregar al carrito
             </button>
             </div>
             <div class="text-center mt-2">
-            <button onclick="detalleProducto(${producto.id})" type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+            <button onclick="detalleProducto(${producto.id})" type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
             Detalles del producto
             </button>
 
